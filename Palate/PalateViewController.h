1c824fd9905e3f3ce6b3961f3a53725ae471c618
Spencer Congero <spencercongero@Spencers-MacBook-Pro.local>
2013-11-02 11:20:06 -0700

    Initial Commit

---
+++ Palate/PalateViewController.h
@@ -0,0 +1,13 @@
+//
+//  PalateViewController.h
+//  Palate
+//
+//  Created by Spencer Congero on 11/2/13.
+//  Copyright (c) 2013 Spencer Congero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PalateViewController : UIViewController
+
+@end
---
+++ Palate/PalateViewController.m
@@ -0,0 +1,29 @@
+//
+//  PalateViewController.m
+//  Palate
+//
+//  Created by Spencer Congero on 11/2/13.
+//  Copyright (c) 2013 Spencer Congero. All rights reserved.
+//
+
+#import "PalateViewController.h"
+
+@interface PalateViewController ()
+
+@end
+
+@implementation PalateViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
